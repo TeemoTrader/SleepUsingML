173dee3c627f165fe0db8918b52ae4be57b9a5f1
TeemoTrader <austinnorman1995@gmail.com>
2021-07-14 16:57:33 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Teemo Norman on 7/14/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
